1f1444cae1aed505dfd36f942c2fd79decf9bf03
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-11-07 13:46:50 -0500

    main method created

--- src/main/java/com/ironyard/Application.java
+++ src/main/java/com/ironyard/Application.java
@@ -1,7 +1,31 @@
 package com.ironyard;
 
+
+import com.ironyard.dto.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * Created by nathanielellsworth on 11/7/16.
  */
+@SpringBootApplication
 public class Application {
+
+    private static Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+
+        Country[] cntry = restTemplate.getForObject("http://api.undata-api.org/WB/GDP%20growth%20(annual%20percent)/countries?app_id=d472233a&app_key=bb0c4591863b91d6208ca6679af8d0e1",Country[].class);
+        log.info(cntry.toString());
+    }
+
+
+
 }
